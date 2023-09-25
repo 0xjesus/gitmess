010f062e7b95bcea6065d2a0f49666b576695151
0xjesus <pdlgmcn@protonmail.com>
2023-09-24 18:13:32 -0600

    📦 feat: add install_gitmess.sh script to automate the installation of gitmess package
    This script checks if pip is installed, if not it installs pip. Then it installs the gitmess package and ensures the gm command is globally available.

---
+++ install_gitmess.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Installing gitmess..."
+
+# Check if pip is installed
+if ! command -v pip &>/dev/null; then
+    echo "pip was not found, installing..."
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python get-pip.py
+    rm get-pip.py
+fi
+
+# Install your package
+pip install gitmess==0.1.0
+
+# Ensure the gm command is globally available
+# This assumes that gitmess provides an entry point named gm after installation.
+if [ ! -f /usr/local/bin/gm ]; then
+    echo '#!/bin/bash' > /usr/local/bin/gm
+    echo 'python -m gitmess "$@"' >> /usr/local/bin/gm
+    chmod +x /usr/local/bin/gm
+fi
+
+echo "gitmess has been successfully installed!"
